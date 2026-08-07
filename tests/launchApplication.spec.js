d08bc4dad755f6b19e8efc5eae7d0400657a718c
Godse <pooja.godse@invest-nl.nl>
2026-08-07 14:45:56 +0200

    New changes

--- tests/launchApplication.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Verify that user can launch the application', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/');
-  
-  const title = await page.title();
-  expect(title).toContain('Practice Software Testing - Toolshop');
-
-})
